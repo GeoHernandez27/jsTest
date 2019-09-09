fdb72cbcafa44222251418231604f63133cdab62
GeoHernandez27 <leopardo2@gmail.com>
2019-09-09 15:36:06 -0600

    fixed timeout issue

--- util.test.js
+++ util.test.js
@@ -3,31 +3,31 @@ const { generateText, checkAndGenerate }  = require('./util');
 
 // Unit Test
 test('should output name and age', () => {
-    const text = generateText('Max', 29);
-    expect(text).toBe('Max (29 years old)');
+    const text = generateText('Geo', 27);
+    expect(text).toBe('Geo (27 years old)');
 
 });
 
 // Integrated test
 test('should generate a valid text ouput', () => {
-    const text = checkAndGenerate('Max', 29);
-    expect(text).toBe('Max (29 years old)');
+    const text = checkAndGenerate('Geo', 27);
+    expect(text).toBe('Geo (27 years old)');
 })
 
 // E2E Test
 test('should create an element with text and correct class', async ()=> {
     const browser = await puppetter.launch({
-        headless: true, 
-        // slowMo: 80,
-        // args: ['--window-size=1920,1080']
+        headless: false, 
+        slowMo: 60,
+        args: ['--window-size=1920,1080']
     });
     const page = await browser.newPage();
     await page.goto('http://127.0.0.1:5500/index.html');
     await page.click('input#name');
-    await page.type('input#name', 'Anna');
+    await page.type('input#name', 'Geo Hernandez');
     await page.click('input#age');
-    await page.type('input#age', '28');
+    await page.type('input#age', '27');
     await page.click('#btnAddUser');
     const finalText = await page.$eval('.user-item', el => el.textContent)
-    expect(finalText).toBe('Anna (28 years old)');
-}), 10000;
+    expect(finalText).toBe('Geo Hernandez (27 years old)');
+}, 10000) ;
